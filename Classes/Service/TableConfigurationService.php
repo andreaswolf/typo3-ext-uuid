afebc2cc9dce082c78c23de01cf40dfda51dc8ff
Andreas Wolf <dev@a-w.io>
2021-07-25 22:06:43 +0200

    [FEATURE] Add field to SQL schema

--- Classes/Service/TableConfigurationService.php
+++ Classes/Service/TableConfigurationService.php
@@ -3,13 +3,19 @@ declare(strict_types = 1);
 
 namespace AndreasWolf\Uuid\Service;
 
+use TYPO3\CMS\Core\Database\Event\AlterTableDefinitionStatementsEvent;
 use TYPO3\CMS\Core\SingletonInterface;
 use TYPO3\CMS\Core\Utility\ExtensionManagementUtility;
 
 class TableConfigurationService implements SingletonInterface
 {
+    /** @var string[] */
+    private $tablesWithUuidField = [];
+
     public function enableUuidForTable(string $tableName): void
     {
+        $this->tablesWithUuidField[] = $tableName;
+
         ExtensionManagementUtility::addTCAcolumns($tableName, [
             'uuid' => [
                 'exclude' => true,
@@ -25,5 +31,18 @@ class TableConfigurationService implements SingletonInterface
             $tableName,
             'uuid'
         );
+    }
+
+    public function addUuidFieldsToDatabaseSchema(AlterTableDefinitionStatementsEvent $event): void
+    {
+        foreach ($this->tablesWithUuidField as $tableName) {
+            $event->addSqlData(<<<SQL
+CREATE TABLE $tableName (
+	uuid VARCHAR(36) DEFAULT NULL,
+
+	KEY uuid (uuid)
+);
+SQL);
+        }
     }
 }
--- Tests/Functional/Service/TableConfigurationServiceTest.php
+++ Tests/Functional/Service/TableConfigurationServiceTest.php
@@ -5,6 +5,7 @@ namespace AndreasWolf\Uuid\Tests\Functional\Service;
 
 use AndreasWolf\Uuid\Service\TableConfigurationService;
 use Nimut\TestingFramework\TestCase\FunctionalTestCase;
+use TYPO3\CMS\Core\Database\Schema\SqlReader;
 use TYPO3\CMS\Core\Utility\GeneralUtility;
 
 class TableConfigurationServiceTest extends FunctionalTestCase
@@ -29,5 +30,20 @@ class TableConfigurationServiceTest extends FunctionalTestCase
         static::assertIsArray($GLOBALS['TCA'][$tableName]['columns']['uuid']);
         static::assertIsArray($GLOBALS['TCA'][$tableName]['types'][0]);
         static::assertStringContainsString('uuid', $GLOBALS['TCA'][$tableName]['types'][0]['showitem']);
+    }
+
+    /** @test */
+    public function schemaMigrationAddsUuidFieldToTable(): void
+    {
+        $tableName = 'tx_testextension_without_uuid';
+        $subject = GeneralUtility::makeInstance(TableConfigurationService::class);
+
+        $subject->enableUuidForTable($tableName);
+
+        $foo = GeneralUtility::makeInstance(SqlReader::class);
+        $result = $foo->getTablesDefinitionString();
+
+        static::assertStringContainsString('uuid VARCHAR(36) DEFAULT NULL', $result);
+        static::assertStringContainsString('KEY uuid (uuid)', $result);
     }
 }
