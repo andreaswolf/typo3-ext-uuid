a630c8adb63c194f5db1e4df0ad970fa9f726b3c
Andreas Wolf <dev@a-w.io>
2021-07-25 16:55:28 +0200

    [TASK] Add basic extension structure

---
+++ ext_emconf.php
@@ -0,0 +1,33 @@
+<?php
+$EM_CONF[$_EXTKEY] = array(
+  'title' => 'UUIDs',
+  'description' => 'Brings UUID functionality to TYPO3 CMS tables',
+  'category' => 'system',
+  'author' => 'Andreas Wolf',
+  'author_email' => 'mail@a-w.io',
+  'author_company' => 'a-w.io',
+  'shy' => '',
+  'priority' => '',
+  'module' => '',
+  'state' => 'stable',
+  'internal' => '',
+  'uploadfolder' => '0',
+  'createDirs' => '',
+  'modify_tables' => '',
+  'clearCacheOnLoad' => 0,
+  'lockType' => '',
+  'version' => '0.1.0',
+  'constraints' =>
+  array(
+    'depends' =>
+    array(
+      'typo3' => '9.5.0-10.4.99',
+    ),
+    'conflicts' =>
+    array(
+    ),
+    'suggests' =>
+    array(
+    ),
+  ),
+);
